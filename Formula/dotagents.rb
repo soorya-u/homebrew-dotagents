a9b50769d88aa15bec5d74be7dab73956e61ce2a
Soorya U <100910996+soorya-u@users.noreply.github.com>
2026-05-04 22:39:53 +0530

    chore: scaffold initial tap structure

---
+++ Formula/dotagents.rb
@@ -0,0 +1,24 @@
+class Dotagents < Formula
+  desc "CLI to manage and template configuration files for AI agents"
+  homepage "https://github.com/soorya-u/dotagents"
+  version "0.1.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/soorya-u/dotagents/releases/download/v#{version}/dotagents-aarch64-apple-darwin"
+      sha256 "PLACEHOLDER_SHA256_MACOS_ARM64"
+    end
+    on_intel do
+      url "https://github.com/soorya-u/dotagents/releases/download/v#{version}/dotagents-x86_64-apple-darwin"
+      sha256 "PLACEHOLDER_SHA256_MACOS_X86"
+    end
+  end
+
+  def install
+    bin.install Dir["dotagents*"].first => "dotagents"
+  end
+
+  test do
+    system "#{bin}/dotagents", "--version"
+  end
+end
